8d84af3ce2e53fbf6df9c6234f0fc4c95f315d99
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-28 17:06:06 +0100

    add responsive image

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 /*Descrizione:
 Partendo dal markup e dall'array presenti nella cartella allegata, creare uno slider di immagini con Vue.
 Milestone 1:
-Modifichiamo il template html per renderizzare dinamicamente la lista delle thumbnails e l'immagine grande (partiamo rendendo attiva la prima immagine dell'array. 
+Modifichiamo il template html per renderizzare dinamicamente la lista delle thumbnails e l'immagine grande 
+(partiamo rendendo attiva la prima immagine dell'array. 
  Quindi l'indice attivo sarà inizialmente 0;
 Milestone2:
 Al click dell'utente sulle frecce incrementare/ decrementare l'indice attivo facendo in modo
@@ -15,31 +16,25 @@ Bonus:
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 
+import { slides } from "./data.js";
 
 
+const {createApp} = Vue;
 
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+createApp({
+    data() {
+        return {
+            slides,
+            active: 0
+        }
+    },
+    methods: {
+        prev(){
+            this.active--
+        },
+        next(){
+            this.active++
+        }
+    },
 
-console.log(slides);
+}).mount('#app')
